45606c5215962eef97fd44ad0186c8a76075d261
nas-be <naserbechata@gmail.com>
2021-06-18 08:39:23 +0200

    added emailTemplates folder and html emails files to be used by the Mailer

---
+++ src/emailTemplates/Fr/fr_confirm_email_update.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+        <style type='text/css'>
+            @import url(http://fonts.googleapis.com/css?family=Open+Sans);
+        </style>
+    </head>
+    <body style=' font-size:16px; color:black;'>
+
+        <div style="position:relative; max-width:700px; min-width:500px; overflow:hidden; margin: 10px auto; padding:10px; border:#27b88e 3px solid; border-radius:10px;" >
+        <div style='text-align:center; font-size:16px;  margin: 0 10%; background-color:white; border-radius: 10px;  overflow:hidden;'>
+        <div> <?php echo  $body;?> </div>
+        <br><hr style='width:150px; height: 2.5px; border: none;color: #636363;background-color: #636363; border-radius: 5px;'>
+       
+        </div>
+        </div>
+       
+
+    </body>
+</html>
