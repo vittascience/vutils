22e4e50281017fabbcf46e3eaf85efd397f43f49
ultimecreation <naserbechata@gmail.com>
2022-11-24 11:20:59 +0100

    updated output  format

--- src/Controller/ControllerProdIssuesNotifier.php
+++ src/Controller/ControllerProdIssuesNotifier.php
@@ -22,7 +22,7 @@ class ControllerProdIssuesNotifier
 
     public function notifySlack()
     {
-        $decodedData = json_decode($_POST['errorReport']); 
+        $decodedData = json_decode($_POST['errorReport']);
         $routingUrlQueryString = explode('?', $decodedData->context->routingUrl)[1];
         $queryParts = explode('&', $routingUrlQueryString);
         $controller = str_replace('controller=', '', $queryParts[0]);
@@ -42,15 +42,17 @@ class ControllerProdIssuesNotifier
         if ($decodedData->responseText === '') {
             $body .= "<p>La réponse du serveur est une string vide. Piste éventuelle: doctrine a pu rencontrer une erreur. Voir les fichiers de logs dans le container docker_web_1";
         }
-        
-        $test =json_encode($decodedData,JSON_PRETTY_PRINT);
-       
+
+        $originalString = json_encode($decodedData, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        $stringTest = str_replace('\\\\n', '\\n', $originalString);        // $stringWithoutDoubleBackslashes = str_replace('\\\\', 'vittapattern', $originalString);
+        // $stringWithoutSingleBackslashe = str_replace('\\', '', $stringWithoutDoubleBackslashes);
+        // $finalStringToDisplay = str_replace('vittapattern', '\\', $stringWithoutSingleBackslashe);
+
         $body .= "
-        <hr>
+            <hr>
             <p>Détail de l'erreur de la requête:</p>
-            <pre style='background: black; color: white; overflow: scroll;'>
-                $test    
-            </pre>";
+            <pre style='background: black;color: white;overflow: scroll;padding: 0.5em 1em;border-radius: 0.75em;font-size: 1.1em;user-select:all;'>$stringTest</pre>";
+
         // send email
         Mailer::sendMail($emailReceiver,  $subject, $body, strip_tags($body), $emailTtemplateBody);
         return;
